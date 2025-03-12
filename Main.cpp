69a34223af93a15b42facdafc93599f7063a5db9
André B <andrevbm.288@gmail.com>
2025-03-12 17:05:27 -0300

    Implementação de objetos 3D e transformação usando GLM

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <math.h>
 
 #include "shaderClass.h"
@@ -8,7 +11,8 @@
 #include "VBO.h"
 #include "EBO.h"
 
-void rotate();
+const unsigned int width = 500;
+const unsigned int height = 500;
 
 float cx = 0.0f;
 float cy = 0.0f;
@@ -75,30 +79,6 @@ GLuint indices[] = {
     21, 22, 23
 };
 
-float xAngle = 0.0f;
-float xRotation[] = {
-    1.0f, 0.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f, 0.0f,
-    0.0f, 0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 0.0f, 1.0f
-};
-
-float yAngle = 0.0f;
-float yRotation[] = {
-    1.0f, 0.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f, 0.0f,
-    0.0f, 0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 0.0f, 1.0f
-};
-
-float zAngle = 0.0f;
-float zRotation[] = {
-    1.0f, 0.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f, 0.0f,
-    0.0f, 0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 0.0f, 1.0f
-};
-
 int main()
 {
     glfwInit();
@@ -106,7 +86,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(500, 500, "OpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(width, height, "OpenGL", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -116,7 +96,7 @@ int main()
     glfwMakeContextCurrent(window);
 
     gladLoadGL();
-    glViewport(0, 0, 500, 500);
+    glViewport(0, 0, width, height);
 
     Shader shaderProgram("default.vert", "default.frag");
 
@@ -129,29 +109,36 @@ int main()
     vao.linkAttrib(vbo, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
     vao.linkAttrib(vbo, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 
-    GLuint xRotLoc = glGetUniformLocation(shaderProgram._id, "xRotation");
-    GLuint yRotLoc = glGetUniformLocation(shaderProgram._id, "yRotation");
-    GLuint zRotLoc = glGetUniformLocation(shaderProgram._id, "zRotation");
-
     vao.unbind();
     vbo.unbind();
     ebo.unbind();
 
+	float time = 0.0f;
+	glEnable(GL_DEPTH_TEST);
+
     while (!glfwWindowShouldClose(window))
     {
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         shaderProgram.activate();
 
-        xAngle += 1 / 6000.0f;
-        yAngle += 1 / 6000.0f;
-        zAngle += 1 / 6000.0f;
-        rotate();
+		glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
 
-        glUniformMatrix4fv(xRotLoc, 1, GL_FALSE, xRotation);
-        glUniformMatrix4fv(yRotLoc, 1, GL_FALSE, yRotation);
-        glUniformMatrix4fv(zRotLoc, 1, GL_FALSE, zRotation);
+		time += 1 / 600.0f;
+		model = glm::rotate(model, time, glm::vec3(0.0f, 1.0f, 0.0f));
+		view = glm::translate(view, glm::vec3(0.0f, -0.4f, -2.0f));
+        proj = glm::perspective(glm::radians(45.0f), (float)(width/height), 0.1f, 100.0f);
+
+        GLuint modelLoc = glGetUniformLocation(shaderProgram._id, "model");
+        GLuint viewLoc = glGetUniformLocation(shaderProgram._id, "view");
+        GLuint projLoc = glGetUniformLocation(shaderProgram._id, "proj");
+
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
 
         vao.bind();
         glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
@@ -168,21 +155,4 @@ int main()
     glfwTerminate();
 
     return 0;
-}
-
-void rotate() {
-    xRotation[5] = cos(xAngle);
-    xRotation[6] = -sin(xAngle);
-    xRotation[9] = sin(xAngle);
-    xRotation[10] = cos(xAngle);
-
-    yRotation[0] = cos(yAngle);
-    yRotation[2] = sin(yAngle);
-    yRotation[8] = -sin(yAngle);
-    yRotation[10] = cos(yAngle);
-
-    zRotation[0] = cos(zAngle);
-    zRotation[1] = -sin(zAngle);
-    zRotation[4] = sin(zAngle);
-    zRotation[5] = cos(zAngle);
 }
--- default.vert
+++ default.vert
@@ -1,13 +1,12 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
-uniform mat4 xRotation;
-uniform mat4 yRotation;
-uniform mat4 zRotation;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
 out vec3 color;
 void main()
 {
+	gl_Position = proj * view * model * vec4(aPos, 1.0f);
 	color = aColor;
-	vec4 pos = zRotation * yRotation * xRotation * vec4(aPos, 1.0f);
-	gl_Position = vec4(pos);
 }
